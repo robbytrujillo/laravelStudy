1e7c917014227a5470d631ac4c74fe3f6a758ccc
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-03-03 04:44:15 +0700

    Fundamental Laravel | Blade Templating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <a class="navbar-brand" href="#">Blade Templating</a>
+          <a class="navbar-brand" href="#"><span style="color: gold">blade</span>Templating.</a>
           <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
@@ -37,10 +37,14 @@
       </nav>
       {{-- Navbar section End --}}
     
+    <div class="container">
     <h1>Halaman Home</h1>
-    <h2>Selamat Datang, <span style="color: darkolivegreen">{{ $name }}. Anda sebagai <span style="color: blue">{{ $role }}</span>!.</span></h2>
+    <h2>Selamat Datang, <span style="color: darkolivegreen">{{ $name }}.</h2>
+    <h2> Anda sebagai <span style="color: blue">{{ $role }}</span>!.</span></h2>
 
     <button class="btn btn-primary">Submit</button>
+    </div>
+
     {{-- JS Bundle --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
